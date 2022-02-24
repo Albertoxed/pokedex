12e58e0d3539c30b5cc0375606047c9b8ee818d0
Albertoxed <Albertojosegarcia71@gmail.com>
2022-02-24 11:56:21 -0500

    comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+
+
+
+const App  = ()=> {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon();
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter pokemon name"
+          />
+        </label>
+      </form>
+     {pokemonData.map((data) => {
+        return(
+          <div className="container">
+            <img src={data.sprites["front_default"]} />
+            
+            <div className= "divTable">  
+              <div className="divTableBody">
+                <div className="divTableRow">
+                  <div className="divTableCell">Type </div>
+                  <div className="divTableCell">{pokemonType} </div>
 
+            
+            <div className="divTableRow">
+                  <div className="divTableCell">Height </div>
+                  <div className="divTableCell">  {" "}
+                    {Math.round(data.height * 3.9)}" </div>
+
+            </div>
+            <div className="divTableRow">
+                  <div className="divTableCell">Weigth </div>
+                  <div className="divTableCell">{""} {Math.round(data.weight/4.3)} lbs </div>
+
+            </div>
+            <div className="divTableRow">
+                  <div className="divTableCell">Number of battles </div>
+                  <div className="divTableCell">{data.game_indices.length}  </div>
+
+            </div>
+
+              </div>
+            </div>
+            
+            </div>
+               
+            
+              
+          </div>
+          
+        )
+        
+     })}
+      </div>
+      
+  )
+  }
+  
 export default App;
